39580b29b7698642d4c9ff5bd3a26d577d16cb9d
IvanBichan <ivanbichanfe@gmail.com>
2022-11-01 17:40:43 +0300

    feat: add solution with map

---
+++ src/NewComponent.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+type NewComponentPropsType = {
+    //students:Array<StudentType>
+    students:StudentType[]
+    topCars:CarType[]
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+}
+type CarType = {
+    manufacturer:string,
+    model:string
+}
+
+export const NewComponent = (props:NewComponentPropsType) =>{
+
+
+    return(
+        <>
+        <div>{props.students.map(
+            (m)=>{return(
+                <div key={m.id}><span>{m.name} - {m.age}</span></div>
+            )}
+        )}
+        </div>
+            <table>
+                {props.topCars.map(
+                    (m,i)=>{
+                        return(
+                                <tr>
+                                    <td>{m.manufacturer}</td>
+                                    <td>{m.model}</td>
+                                </tr>
+                        )
+                    }
+                )}
+            </table>
+        </>
+    )
+}
+
+
